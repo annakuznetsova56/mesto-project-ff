e6e097bcb8e2b5fce6eb1df9f17b39f512ff9b5a
Anna Smith <annakuznetsova56123@gmail.com>
2025-05-18 15:26:45 +0300

    первая версия проекта

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+function addCard (name, link, deleteFunc) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunc);
 
-// @todo: DOM узлы
+    return cardElement;
+}
 
-// @todo: Функция создания карточки
+function deleteFunction (button) {
+    button.target.closest('.card').remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const cards = document.querySelector('.places__list');
+initialCards.forEach(function (item) {
+    const newcard = addCard(item.name, item.link, deleteFunction);
+    cards.append(newcard);
+})
